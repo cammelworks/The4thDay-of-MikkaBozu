f4aa24d48b671d8435fb29044292b49a30bf2409
iHiromu <inahiro.911@gmail.com>
2020-06-17 21:17:22 +0900

    numberpickerを追加しました

--- lib/team_page.dart
+++ lib/team_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:numberpicker/numberpicker.dart';
 
 class TeamPage extends StatefulWidget {
   String _TeamName;
@@ -21,7 +21,31 @@ class TeamPageState extends State<TeamPage> {
       appBar: AppBar(
         title: Text(_TeamName),
       ),
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    '目標',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                  child: NumberPicker.integer(
+                initialValue: 50,
+                minValue: 0,
+                maxValue: 100,
+              )),
+            ]),
+      ),
     );
   }
-
 }
