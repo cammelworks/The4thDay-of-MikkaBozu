ede883d6b71abb91e199b0948f48d43b842b0f0c
TakedaHiroki <hirokitakedaxxx@gmail.com>
2020-06-26 17:44:21 +0900

    サイドメニューに所属しているチームの一覧を表示

--- lib/Pages/MyPage/SideMenu/sidemenu.dart
+++ lib/Pages/MyPage/SideMenu/sidemenu.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../../LookupTeamPage/lookup_team_page.dart';
@@ -11,6 +12,7 @@ class Sidemenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
+        shrinkWrap: true,
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
@@ -23,37 +25,80 @@ class Sidemenu extends StatelessWidget {
                 image: DecorationImage(
                     fit: BoxFit.fill, image: AssetImage('images/bouzu.png'))),
           ),
-          ListTile(
-            title: Text('MYPAGE'),
-            onTap: () => {},
-          ),
-          ListTile(
-            title: Text('TEAM'),
-            trailing: Wrap(
-              spacing: -16,
-              children: <Widget>[
-                IconButton(icon: Icon(Icons.search),
-                onPressed: () async {
-                    //チーム作成ページに移動
-                  final result = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                  builder: (context) => LookupTeamPage(_email),
-                  ));
-                  }),
-                  IconButton(icon: Icon(Icons.add),
-                  onPressed: () async {
-                    //チーム作成ページに移動
-                    final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                    builder: (context) => TeamCreatePage(_email),
-                    ));
-                    }),
-            ]),
+          Container(
+            height: 34,
+            child: ListTile(
+              title: Text('MYPAGE'),
+              onTap: () => {},
             ),
-          ],
-        ),
-      );
+          ),
+          Container(
+            height: 34,
+            child: ListTile(
+              title: Text('TEAM',
+              ),
+              trailing: Wrap(spacing: -16, children: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () async {
+                      //チーム作成ページに移動
+                      final result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LookupTeamPage(_email),
+                          ));
+                    }),
+                IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () async {
+                      //チーム作成ページに移動
+                      final result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TeamCreatePage(_email),
+                          ));
+                    }),
+              ]),
+            ),
+          ),
+          Container(
+            child: StreamBuilder<QuerySnapshot>(
+                //表示したいFirestoreの保存先を指定
+                stream: Firestore.instance
+                    .collection('users')
+                    .document(_email)
+                    .collection('teams')
+                    .snapshots(),
+                //streamが更新されるたびに呼ばれる
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  //データが取れていない時の処理
+                  if (!snapshot.hasData) return const Text('Loading...');
+                  return ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) => _buildListItem(
+                          context, snapshot.data.documents[index]));
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            height: 34,
+            child: ListTile(title: Text(document['team_name'])),
+          )
+        ],
+      ),
+    );
   }
 }
