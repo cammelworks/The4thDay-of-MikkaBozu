056671dee97822c4b21a7efbde834c10cb61bb6a
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-06-27 23:38:49 +0900

    1秒ごとに緯度経度を取得し、設定した点との距離を計算する

--- lib/Pages/MeasurementPage/measurement_page.dart
+++ lib/Pages/MeasurementPage/measurement_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class MeasurementPage extends StatefulWidget {
   @override
@@ -15,10 +16,14 @@ class MeasurementPageState extends State<MeasurementPage> {
   @override
   void initState() {
     super.initState();
-    _getLocation(context);
+    _getLocation();
+    Timer.periodic(
+      Duration(seconds: 1),
+      countTime,
+    );
   }
 
-  Future<void> _getLocation(context) async {
+  Future<void> _getLocation() async {
     Position _currentPosition = await Geolocator().getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high); // ここで精度を「high」に指定している
     print(_currentPosition);
@@ -87,13 +92,41 @@ class MeasurementPageState extends State<MeasurementPage> {
         });
   }
 
+  void countTime(Timer timer) async {
+    await _getLocation();
+    showLocation();
+  }
+
   Widget showLocation() {
     if (_showLocation) {
       //登録ボタンの表示
-      return Text("${position}");
+      return Column(
+        children: <Widget>[
+          Text("${position}"),
+          FutureBuilder(
+            future: getDistance(),
+            builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
+              if (snapshot.hasData) {
+                return snapshot.data;
+              } else {
+                return Center(child: Text("距離計算中"));
+              }
+            },
+          ),
+//          Text(await Geolocator().distanceBetween(position.latitude, position.longitude, 34.4015, 132.714))
+        ],
+      );
     } else {
       //何も表示しない
       return null;
     }
+  }
+
+  Future<Widget> getDistance() async {
+    double distance = await Geolocator()
+        .distanceBetween(
+        position.latitude, position.longitude, 34.4015, 132.714);
+    print(distance);
+    return Text("${distance}");
   }
 }
