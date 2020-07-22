d26623a065e9a4909c570f4646fe81016a46dd24
NishidaYujiro <b173033@hiroshima-u.ac.jp>
2020-07-22 19:07:52 +0900

    各ページのボタンや表示などを簡単に作成する

--- lib/Pages/RunPage/run_page.dart
+++ lib/Pages/RunPage/run_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:the4thdayofmikkabozu/Pages/RunPage/to_mesurement_page_button.dart';
+import 'package:the4thdayofmikkabozu/SideMenu/sidemenu.dart';
 
 class RunPage extends StatefulWidget {
   @override
@@ -12,6 +14,13 @@ class RunPageState extends State<RunPage> {
       appBar: AppBar(
         title: Text("ランページ"),
       ),
+      body: Column(
+        children: <Widget>[
+          Text("走る準備ができたら押してください"),
+          ToMeasurementPageButton()
+        ],
+      ),
+      drawer: Sidemenu(),
     );
   }
 }
--- lib/Pages/TeamMainPage/team_main_page.dart
+++ lib/Pages/TeamMainPage/team_main_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:the4thdayofmikkabozu/Pages/LookupTeamPage/lookup_team_page.dart';
+import 'package:the4thdayofmikkabozu/Pages/TeamCreatePage/team_create_page.dart';
+import 'package:the4thdayofmikkabozu/Pages/TeamPage/team_page.dart';
+import 'package:the4thdayofmikkabozu/SideMenu/sidemenu.dart';
+import 'package:the4thdayofmikkabozu/user_data.dart' as userData;
 
 class TeamMainPage extends StatefulWidget {
   @override
@@ -6,12 +12,89 @@ class TeamMainPage extends StatefulWidget {
 }
 
 class TeamMainPageState extends State<TeamMainPage> {
+  String _email = userData.userEmail;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("チーム関連ページ"),
       ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Row(
+              children: <Widget>[
+                Spacer(),
+                Text("チーム検索"),
+                Icon(Icons.search),
+                Spacer()
+              ],
+            ),
+            onPressed: () async {
+              await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LookupTeamPage(),
+                  )
+              );
+            },
+          ),
+          RaisedButton(
+            child: Row(
+              children: <Widget>[
+                Spacer(),
+                Text("チーム作成"),
+                Icon(Icons.add),
+                Spacer()
+              ],
+            ),
+            onPressed: () async {
+              await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TeamCreatePage(),
+                  )
+              );
+            },
+          ),
+          Container(
+            child: StreamBuilder<QuerySnapshot>(
+              //表示したいFirestoreの保存先を指定
+                stream: Firestore.instance
+                    .collection('users')
+                    .document(_email)
+                    .collection('teams')
+                    .snapshots(),
+                //streamが更新されるたびに呼ばれる
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  //データが取れていない時の処理
+                  if (!snapshot.hasData) return const Text('Loading...');
+                  return ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) => _buildListItem(
+                          context, snapshot.data.documents[index]['team_name']));
+                }),
+          ),
+        ],
+      ),
+      drawer: Sidemenu(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, String teamName) {
+    return RaisedButton(
+      child: Text(teamName),
+      onPressed: () async {
+        await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TeamPage(teamName),
+            )
+        );
+      },
     );
   }
 }
