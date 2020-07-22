c155f52c1b66add50316cac4439eefb9c0544626
NishidaYujiro <b173033@hiroshima-u.ac.jp>
2020-07-22 18:31:36 +0900

    PageViewからアクセスするページを作成する(ランページ、チーム関連ページ)

---
+++ lib/Pages/RunPage/run_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RunPage extends StatefulWidget {
+  @override
+  RunPageState createState() => RunPageState();
+}
+
+class RunPageState extends State<RunPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ランページ"),
+      ),
+    );
+  }
+}
---
+++ lib/Pages/TeamMainPage/team_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TeamMainPage extends StatefulWidget {
+  @override
+  TeamMainPageState createState() => TeamMainPageState();
+}
+
+class TeamMainPageState extends State<TeamMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("チーム関連ページ"),
+      ),
+    );
+  }
+}
