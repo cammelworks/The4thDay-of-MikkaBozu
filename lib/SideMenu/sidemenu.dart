2e63059f9f5c5e6d9889281bf8a90faa6e0e65de
NishidaYujiro <b173033@hiroshima-u.ac.jp>
2020-07-22 18:36:32 +0900

    sidemenu.dartの不要なimport文を削除

--- lib/SideMenu/sidemenu.dart
+++ lib/SideMenu/sidemenu.dart
@@ -1,14 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:the4thdayofmikkabozu/SideMenu/signout_button.dart';
-import 'package:the4thdayofmikkabozu/user_data.dart' as userData;
-import 'package:the4thdayofmikkabozu/Pages/LookupTeamPage/lookup_team_page.dart';
-import 'package:the4thdayofmikkabozu/Pages/TeamcreatePage/team_create_page.dart';
-import 'package:the4thdayofmikkabozu/Pages/TeamPage/team_page.dart';
-import 'package:the4thdayofmikkabozu/Pages/MyPage/my_page.dart';
 
 class Sidemenu extends StatelessWidget {
-  String _email = userData.userEmail;
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -30,6 +23,4 @@ class Sidemenu extends StatelessWidget {
       ),
     );
   }
-
-
 }
