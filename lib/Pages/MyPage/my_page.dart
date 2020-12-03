b9c22e3f852a0aed11d3787aa142c32589a31f1c
NishidaYujiro <b173033@hiroshima-u.ac.jp>
2020-12-03 19:40:52 +0900

    my_pageにカレンダーを表示する

--- lib/Pages/MyPage/my_page.dart
+++ lib/Pages/MyPage/my_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
 import 'package:the4thdayofmikkabozu/SideMenu/sidemenu.dart';
-import 'package:the4thdayofmikkabozu/Pages/MyPage/records_screen.dart';
 
 class MyPage extends StatefulWidget {
   final String title = '記録ページ';
@@ -11,6 +12,12 @@ class MyPage extends StatefulWidget {
 }
 
 class MyPageState extends State<MyPage>{
+  DateTime _currentDate = DateTime.now();
+
+  void onDayPressed(DateTime date, List<Event> events) {
+    this.setState(() => _currentDate = date);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +31,26 @@ class MyPageState extends State<MyPage>{
             children: <Widget>[
               Container(
                 child: Center(
-                  //距離のデータを表示
-                  child: RecordsScreen(),
+                  child: CalendarCarousel<Event>(
+                        onDayPressed: onDayPressed,
+                        weekendTextStyle: TextStyle(color: Colors.red),
+                        thisMonthDayBorderColor: Colors.grey,
+                        weekFormat: false,
+                        height: 420.0,
+                        selectedDateTime: _currentDate,
+                        daysHaveCircularBorder: false,
+                        customGridViewPhysics: NeverScrollableScrollPhysics(),
+                        markedDateShowIcon: true,
+                        markedDateIconMaxShown: 2,
+                        todayTextStyle: TextStyle(
+                          color: Colors.blue,
+                        ),
+                        markedDateIconBuilder: (event) {
+                          return event.icon;
+                        },
+                        todayBorderColor: Colors.green,
+                        markedDateMoreShowTotal: false
+                  ),
                 ),
               ),
             ],
