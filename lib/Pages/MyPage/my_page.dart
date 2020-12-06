6945d5f89f18dff2b9c12d1c28062453d16e2846
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-12-06 21:11:46 +0900

    選択した日付の記録をカレンダーの下に表示する

--- lib/Pages/MyPage/my_page.dart
+++ lib/Pages/MyPage/my_page.dart
@@ -10,19 +10,26 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 class MyPage extends StatefulWidget {
   final String title = '記録ページ';
+
   MyPage();
+
   @override
   State<StatefulWidget> createState() => MyPageState();
 }
 
-class MyPageState extends State<MyPage>{
+class MyPageState extends State<MyPage> {
   DateTime _currentDate = DateTime.now();
   EventList<Event> _markedDateMap = EventList<Event>();
+  bool _shouldShowRecord = false;
+  String _selectedRecord = "";
 
   void onDayPressed(DateTime date, List<Event> events) {
     this.setState(() => _currentDate = date);
-    if(events.length > 0){
-      Fluttertoast.showToast(msg: events[0].title + "km");
+    if (events.length > 0) {
+      _shouldShowRecord = true;
+      _selectedRecord = events[0].title + "km";
+    } else {
+      _shouldShowRecord = false;
     }
   }
 
@@ -43,44 +50,47 @@ class MyPageState extends State<MyPage>{
         .getDocuments();
 
     double max_distance;
-    for(int i=0; i<snapshots.documents.length; i++){
-      if(i == 0){
+    for (int i = 0; i < snapshots.documents.length; i++) {
+      if (i == 0) {
         max_distance = snapshots.documents[i].data['distance'] as double;
       }
-      DateTime date = (snapshots.documents[i].data['timestamp'] as Timestamp).toDate();
+      DateTime date =
+          (snapshots.documents[i].data['timestamp'] as Timestamp).toDate();
       double distance = snapshots.documents[i].data['distance'] as double;
       double roundedDistance = (distance / 100).round() / 10;
-      addEvent(getDate(date), roundedDistance, getColorCode(max_distance, distance));
+      addEvent(
+          getDate(date), roundedDistance, getColorCode(max_distance, distance));
     }
     this.setState(() {});
   }
 
   // 時間情報を取り除く
-  DateTime getDate(DateTime date){
+  DateTime getDate(DateTime date) {
     int year = date.year;
     int month = date.month;
     int day = date.day;
-    return(DateTime(year, month, day));
+    return (DateTime(year, month, day));
   }
 
-  String getColorCode(double max, double distance){
+  String getColorCode(double max, double distance) {
     int tmp = ((1 - distance / max) * 100).round();
     return '00' + tmp.toString().padLeft(2, '0') + 'c4';
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _markedDateMap.clear();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body:SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -88,27 +98,61 @@ class MyPageState extends State<MyPage>{
               Container(
                 child: Center(
                   child: CalendarCarousel<Event>(
-                        onDayPressed: onDayPressed,
-                        weekendTextStyle: TextStyle(color: Colors.red),
-                        thisMonthDayBorderColor: Colors.grey,
-                        weekFormat: false,
-                        height: 420.0,
-                        todayButtonColor: Colors.white,
-                        selectedDateTime: _currentDate,
-                        daysHaveCircularBorder: false,
-                        customGridViewPhysics: NeverScrollableScrollPhysics(),
-                        markedDatesMap: _markedDateMap,
-                        markedDateShowIcon: true,
-                        markedDateIconMaxShown: 2,
-                        locale: "ja",
-                        todayTextStyle: TextStyle(
-                          color: Colors.blue,
-                        ),
-                        markedDateIconBuilder: (event) {
-                          return event.icon;
-                        },
-                        todayBorderColor: Colors.green,
-                        markedDateMoreShowTotal: false,
+                    onDayPressed: onDayPressed,
+                    weekendTextStyle: TextStyle(color: Colors.red),
+                    thisMonthDayBorderColor: Colors.grey,
+                    weekFormat: false,
+                    height: 420.0,
+                    todayButtonColor: Colors.white,
+                    selectedDateTime: _currentDate,
+                    daysHaveCircularBorder: false,
+                    customGridViewPhysics: NeverScrollableScrollPhysics(),
+                    markedDatesMap: _markedDateMap,
+                    markedDateShowIcon: true,
+                    markedDateIconMaxShown: 2,
+                    locale: "ja",
+                    todayTextStyle: TextStyle(
+                      color: Colors.blue,
+                    ),
+                    markedDateIconBuilder: (event) {
+                      return event.icon;
+                    },
+                    todayBorderColor: Colors.green,
+                    markedDateMoreShowTotal: false,
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: _shouldShowRecord,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.blue, width: 1.0)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          padding:
+                              EdgeInsets.fromLTRB(size.width / 10, 20, 10, 20),
+                          child: Text(
+                            _currentDate.month.toString() +
+                                '月' +
+                                _currentDate.day.toString() +
+                                '日',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          )),
+                      Container(
+                          padding:
+                              EdgeInsets.fromLTRB(10, 6, size.width / 10, 6),
+                          child: Text(
+                            _selectedRecord,
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          )),
+                    ],
                   ),
                 ),
               ),
@@ -121,28 +165,28 @@ class MyPageState extends State<MyPage>{
   }
 
   void addEvent(DateTime date, double distance, String colorCode) {
-    print(colorCode);
     _markedDateMap.add(date, createEvent(date, distance, colorCode));
-  }  // 追加
+  } // 追加
 
   Event createEvent(DateTime date, double distance, String colorCode) {
     return Event(
-        date: date,
-        title: distance.toString(),
-        icon: Container(
-          color: HexColor(colorCode),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(date.day.toString()),
-              Text(distance.toString() + "km"),
-            ],
-          ),
+      date: date,
+      title: distance.toString(),
+      icon: Container(
+        color: HexColor(colorCode),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(date.day.toString()),
+            Text(distance.toString() + "km"),
+          ],
         ),
+      ),
     );
   } // 追加
 }
 
+// カラーコードからColorを生成する
 class HexColor extends Color {
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll('#', '');
@@ -151,6 +195,5 @@ class HexColor extends Color {
     }
     return int.parse(hexColor, radix: 16);
   }
-
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
