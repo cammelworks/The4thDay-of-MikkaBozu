db79cb8fa308476dec50a72cd7552ccf43eb86d5
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-12-11 20:28:41 +0900

    各チームメンバーの今週走った距離を取得する

--- lib/Pages/TeamPage/members_record.dart
+++ lib/Pages/TeamPage/members_record.dart
@@ -33,85 +33,162 @@ class MembersRecord extends StatelessWidget {
           if (!snapshot.hasData) return const Text('Loading...');
           final Size size = MediaQuery.of(context).size;
           int achievementNum = 0;
-          return Column(
-            children: [
-              Text(
-                '到達人数',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
-              Text(
-                achievementNum.toString() + "/" + snapshot.data.documents.length.toString(),
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                ),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: new LinearPercentIndicator(
-                      width: MediaQuery.of(context).size.width - 50,
-                      animation: true,
-                      lineHeight: 20.0,
-                      animationDuration: 2000,
-                      percent: achievementNum/snapshot.data.documents.length,
-//                      center: Text("90.0%"),
-                      linearStrokeCap: LinearStrokeCap.roundAll,
-                      progressColor: hex.HexColor("f17300"),
-                      trailing: Image.asset(
-                        'images/flag-icon.png',
-                        height: 30.0,
-                        width: 30.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                'メンバー',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
-              Container(
-                height: size.height * (1 / 3),
-                child: Scrollbar(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data.documents.length,
-                    itemBuilder: (context, int index) {
-                      String userEmail =
-                          snapshot.data.documents[index].documentID.toString();
-                      if (userEmail == userData.userEmail) {
-                        return Container();
-                      }
-                      return ListTile(
-                        leading: Icon(
-                          Icons.account_circle,
-                          size: 50,
-                        ),
-                        title: Text(userEmail),
-                        onTap: () => Navigator.push<dynamic>(
-                            context,
-                            MaterialPageRoute<dynamic>(
-                              builder: (context) => MemberPage(snapshot
-                                  .data.documents[index].documentID
-                                  .toString()),
-                            )),
+
+          //チームの目標の取得
+          return FutureBuilder(
+            future: getGoal(),
+            builder: (BuildContext context, AsyncSnapshot<int> goalSnapshot) {
+              if (goalSnapshot.hasData) {
+                // 個人の記録の取得
+                return FutureBuilder(
+                  future: getMemberRecord(snapshot.data, goalSnapshot.data),
+                  builder: (BuildContext context, AsyncSnapshot<Map> memberSnapshot) {
+                    if (memberSnapshot.hasData) {
+                      print(memberSnapshot.data);
+                      int achievedMemberNum = 0;
+                      memberSnapshot.data.forEach((dynamic key, dynamic value) {
+                        if(value as bool){
+                          achievementNum++;
+                        }
+                      });
+                      return Column(
+                        children: [
+                          Text(
+                            '到達人数',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                          Text(
+                            achievementNum.toString() + "/" + snapshot.data.documents.length.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: new LinearPercentIndicator(
+                                  width: MediaQuery.of(context).size.width - 50,
+                                  animation: true,
+                                  lineHeight: 20.0,
+                                  animationDuration: 2000,
+                                  percent: achievementNum/snapshot.data.documents.length,
+            //                      center: Text("90.0%"),
+                                  linearStrokeCap: LinearStrokeCap.roundAll,
+                                  progressColor: hex.HexColor("f17300"),
+                                  trailing: Image.asset(
+                                    'images/flag-icon.png',
+                                    height: 30.0,
+                                    width: 30.0,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'メンバー',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                          Container(
+                            height: size.height * (1 / 3),
+                            child: Scrollbar(
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: snapshot.data.documents.length,
+                                itemBuilder: (context, int index) {
+                                  String userEmail =
+                                      snapshot.data.documents[index].documentID.toString();
+                                  if (userEmail == userData.userEmail) {
+                                    return Container();
+                                  }
+                                  return ListTile(
+                                    leading: Icon(
+                                      Icons.account_circle,
+                                      size: 50,
+                                    ),
+                                    title: Text(userEmail),
+                                    onTap: () => Navigator.push<dynamic>(
+                                        context,
+                                        MaterialPageRoute<dynamic>(
+                                          builder: (context) => MemberPage(snapshot
+                                              .data.documents[index].documentID
+                                              .toString()),
+                                        )),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
                       );
-                    },
-                  ),
-                ),
-              ),
-            ],
+                    } else {
+                      return Text("record loading...");
+                    }
+                  });
+              } else{
+                return Text("goal loading...");
+              }
+            },
           );
         });
+  }
+
+  Future<int> getGoal() async{
+    var snapshot = await Firestore.instance
+        .collection('teams')
+        .document((_teamName))
+        .get();
+    return snapshot.data['goal'] as int;
+  }
+
+  Future<Map> getMemberRecord(QuerySnapshot data, int goal) async{
+    // マップの初期化
+    Map<String, bool> hasMemberAchieved = {};
+    for(int i=0; i<data.documents.length; i++){
+      double totalDistance = 0.0;
+      QuerySnapshot snapshots = await Firestore.instance
+          .collection('users')
+          .document(data.documents[i].documentID)
+          .collection('records')
+          .where("timestamp", isLessThan: Timestamp.fromDate(getLastSundayDataTime()))
+          .getDocuments();
+      for(int j=0; j<snapshots.documents.length; j++){
+        totalDistance += snapshots.documents[j].data['distance'] as double;
+      }
+      // 目標を達成しているかの確認
+      if(((totalDistance / 100.0).round() / 10) >= goal){
+        hasMemberAchieved[data.documents[i].documentID] = true;
+      } else{
+        hasMemberAchieved[data.documents[i].documentID] = false;
+      }
+    }
+    return hasMemberAchieved;
+  }
+
+  // 直近の日曜日の日付を算出し、その日付を返す
+  DateTime getLastSundayDataTime() {
+    DateTime dResult = DateTime.now();
+
+    // 当日が日曜日ではないならば、
+    // 直前の日曜日まで日付を戻していく
+    if (dResult.weekday != DateTime.sunday) {
+      for (int i = 0; i < 7; ++i) {
+        dResult = dResult.subtract(Duration(days : 1));
+        if (dResult.weekday == DateTime.sunday) {
+          break;
+        }
+      }
+    }
+
+    return (dResult);
   }
 }
