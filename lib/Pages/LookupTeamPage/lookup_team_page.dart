ce20c898beba4fe9ab02e7cf556f9c8e7ebc60ce
TakedaHiroki <hirokitakedaxxx@gmail.com>
2020-12-23 18:40:17 +0900

    チーム作成とチーム検索への遷移ボタンを変更しました

--- lib/Pages/LookupTeamPage/lookup_team_page.dart
+++ lib/Pages/LookupTeamPage/lookup_team_page.dart
@@ -3,6 +3,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:the4thdayofmikkabozu/user_data.dart' as userData;
 import 'package:the4thdayofmikkabozu/Pages/LookupTeamPage/join_button.dart';
+import 'package:the4thdayofmikkabozu/Pages/TeamCreatePage/team_create_page.dart';
 
 class LookupTeamPage extends StatefulWidget {
   final String title = 'チーム検索';
@@ -30,65 +31,98 @@ class LookupTeamPageState extends State<LookupTeamPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Form(
-                key: _formKey,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+//            height: MediaQuery.of(context).size.height,
+              child: SingleChildScrollView(
                 child: Column(
                   children: <Widget>[
-                    TextFormField(
-                      controller: _teamNameField,
-                      decoration: InputDecoration(
-                        labelText: '参加したいチーム名を入力してください',
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.search),
-                          onPressed: () async {
-                            print(_formKey.currentState.validate());
-                            if (_formKey.currentState.validate()) {
-                              // 再レンダリング
-                              setState(() {});
-                            }
-                            //キーボードを閉じる
-                            FocusScope.of(context).requestFocus(new FocusNode());
-                          },
-                        ),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _teamNameField,
+                            decoration: InputDecoration(
+                              labelText: '参加したいチーム名を入力してください',
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.search),
+                                onPressed: () async {
+                                  print(_formKey.currentState.validate());
+                                  if (_formKey.currentState.validate()) {
+                                    // 再レンダリング
+                                    setState(() {});
+                                  }
+                                  //キーボードを閉じる
+                                  FocusScope.of(context)
+                                      .requestFocus(new FocusNode());
+                                },
+                              ),
+                            ),
+                            //エンターアイコンを変更
+                            textInputAction: TextInputAction.search,
+                            onFieldSubmitted: (String value) async {
+                              if (_formKey.currentState.validate())
+                                setState(() {});
+                              ;
+                            },
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return 'チーム名を入力してください';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
                       ),
-                      //エンターアイコンを変更
-                      textInputAction: TextInputAction.search,
-                      onFieldSubmitted: (String value) async {
-                        if (_formKey.currentState.validate()) setState(() {});;
-                      },
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'チーム名を入力してください';
-                        }
-                        return null;
-                      },
                     ),
+                    _showsearchResults(),
                   ],
                 ),
               ),
-              _showsearchResults(),
-            ],
+            ),
           ),
-        ),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 64.0),
+              child: ButtonTheme(
+                minWidth: 200.0,
+                height: 50.0,
+                buttonColor: Colors.white,
+                child: RaisedButton(
+                  child: const Text('新規作成'),
+                  shape: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  onPressed: () async {
+                    await Navigator.push<dynamic>(
+                        context,
+                        MaterialPageRoute<dynamic>(
+                          builder: (context) => TeamCreatePage(),
+                        ));
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _showsearchResults(){
-    if(_teamNameField.text != ""){
+  Widget _showsearchResults() {
+    if (_teamNameField.text != "") {
       return FutureBuilder(
         future: _search(),
-        builder: (BuildContext context, AsyncSnapshot<List<DocumentSnapshot>> snapshots){
+        builder: (BuildContext context,
+            AsyncSnapshot<List<DocumentSnapshot>> snapshots) {
           final Size size = MediaQuery.of(context).size;
           if (snapshots.hasData) {
-            if(snapshots.data.length == 0){
+            if (snapshots.data.length == 0) {
               return Text("該当チームなし");
-            } else{
+            } else {
               return Container(
                 height: size.height * (2 / 3),
                 child: Scrollbar(
@@ -100,35 +134,46 @@ class LookupTeamPageState extends State<LookupTeamPage> {
                           child: Column(
                             children: [
                               Text(
-                                "チーム名 " + snapshots.data[index].data["team_name"].toString(),
+                                "チーム名 " +
+                                    snapshots.data[index].data["team_name"]
+                                        .toString(),
                                 style: TextStyle(
                                   fontSize: 20,
                                 ),
                               ),
                               Visibility(
-                                visible: snapshots.data[index].data["team_overview"]!=null,
+                                visible: snapshots
+                                        .data[index].data["team_overview"] !=
+                                    null,
                                 child: Text(
-                                  "概要 " + snapshots.data[index].data["team_overview"].toString(),
+                                  "概要 " +
+                                      snapshots
+                                          .data[index].data["team_overview"]
+                                          .toString(),
                                   style: TextStyle(
                                     fontSize: 20,
                                   ),
                                 ),
                               ),
                               Visibility(
-                                visible: snapshots.data[index].data["goal"]!=null,
+                                visible:
+                                    snapshots.data[index].data["goal"] != null,
                                 child: Text(
-                                  "目標 " + snapshots.data[index].data["goal"].toString() + "km",
+                                  "目標 " +
+                                      snapshots.data[index].data["goal"]
+                                          .toString() +
+                                      "km",
                                   style: TextStyle(
                                     fontSize: 20,
                                   ),
                                 ),
                               ),
-                              JoinButton(snapshots.data[index].data["team_name"].toString()),
+                              JoinButton(snapshots.data[index].data["team_name"]
+                                  .toString()),
                             ],
                           ),
                         );
-                      }
-                  ),
+                      }),
                 ),
               );
             }
@@ -137,7 +182,7 @@ class LookupTeamPageState extends State<LookupTeamPage> {
           }
         },
       );
-    } else{
+    } else {
       return Container();
     }
   }
@@ -146,12 +191,11 @@ class LookupTeamPageState extends State<LookupTeamPage> {
     //結果を格納するリストを初期化する
     List<DocumentSnapshot> result = [];
     // 全チームを検索する
-    QuerySnapshot docs = await Firestore.instance
-        .collection("teams")
-        .getDocuments();
+    QuerySnapshot docs =
+        await Firestore.instance.collection("teams").getDocuments();
     // すでに参加しているチームをリストに格納する
     await docs.documents.forEach((doc) {
-      if(_teamSearch(doc)){
+      if (_teamSearch(doc)) {
         result.add(doc);
       }
     });
@@ -175,13 +219,14 @@ class LookupTeamPageState extends State<LookupTeamPage> {
     // すでに参加していたらfalseを返す
     if (_joinedTeamList.contains(snapshot.data["team_name"].toString())) {
       return false;
-    }
-    else {
+    } else {
       // チーム名か概要に入力された文字列が含まれていたらtrueを返す
       if (snapshot.data["team_name"].toString().contains(_teamNameField.text)) {
         return true;
-      } else if(snapshot.data["team_overview"] != null &&
-          snapshot.data["team_overview"].toString().contains(_teamNameField.text)){
+      } else if (snapshot.data["team_overview"] != null &&
+          snapshot.data["team_overview"]
+              .toString()
+              .contains(_teamNameField.text)) {
         return true;
       }
       // ヒットなし
