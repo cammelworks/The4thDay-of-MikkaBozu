86bbb976fd456512d3e2ee4d41fbc5f4f6fbe304
tyanio <tyanio1996@gmail.com>
2020-12-23 20:08:56 +0900

    ちょっと整えました

--- lib/Pages/LookupTeamPage/team_card.dart
+++ lib/Pages/LookupTeamPage/team_card.dart
@@ -2,52 +2,62 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:the4thdayofmikkabozu/Pages/LookupTeamPage/join_button.dart';
 
-class TeamCard extends StatelessWidget{
+class TeamCard extends StatelessWidget {
   DocumentSnapshot _snapshot;
-  TeamCard(this._snapshot): super();
+  TeamCard(this._snapshot) : super();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Card(
-        margin: EdgeInsets.all(10.0),
+        margin: EdgeInsets.all(8.0),
         child: Container(
-          width: 200,
           height: 100,
-          child: Row(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text(_snapshot.data["team_name"].toString()),
-                  TeamOverview(_snapshot.data["team_overview"].toString()),
-                ],
-              ),
-              Spacer(),
-              Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      teamGoal(_snapshot.data["goal"].toString()),
-                      teamMemberNum(_snapshot.data["user_num"].toString())
-                    ],
-                  ),
-                  JoinButton(_snapshot.data["team_name"].toString()),
-                ],
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      _snapshot.data["team_name"].toString(),
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    TeamOverview(_snapshot.data["team_overview"].toString()),
+                  ],
+                ),
+                Spacer(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        teamGoal(_snapshot.data["goal"].toString()),
+                        teamMemberNum(_snapshot.data["user_num"].toString())
+                      ],
+                    ),
+                    Spacer(),
+                    JoinButton(_snapshot.data["team_name"].toString()),
+                  ],
+                ),
+              ],
+            ),
           ),
-        )
+        ));
+  }
+
+  Widget TeamOverview(String teamOverview) {
+    if (teamOverview == "null") {
+      return Container();
+    }
+    return Text(
+      "概要：" + teamOverview,
+      style: TextStyle(color: Colors.black54),
     );
   }
 
-  Widget TeamOverview(String teamOverview){
-    if(teamOverview == "null"){
-      return Container();
-    }
-    return Text("概要：" + teamOverview);
-  }
-
   Widget teamGoal(String goal) {
-    if(goal == "null"){
+    if (goal == "null") {
       return Container();
     }
     return Row(
@@ -63,17 +73,14 @@ class TeamCard extends StatelessWidget{
   }
 
   Widget teamMemberNum(String userNum) {
-    if(userNum == "null"){
+    if (userNum == "null") {
       return Container();
     }
     return Row(
       children: <Widget>[
-        Icon(
-            Icons.people_outline
-        ),
+        Icon(Icons.people_outline),
         Text(userNum + "人"),
       ],
     );
   }
 }
-
