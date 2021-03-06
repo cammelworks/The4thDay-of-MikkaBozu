67aa8fd8662038914e10c1e637fbd202c5d12cf0
Tyanio <tyanio1996@gmail.com>
2021-03-06 19:07:32 +0900

    ローカルからの画像取得・表示ができました

--- lib/SideMenu/sidemenu.dart
+++ lib/SideMenu/sidemenu.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_image/flutter_native_image.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:the4thdayofmikkabozu/Pages/IconSelectPage/icon_select_page.dart';
 import 'package:the4thdayofmikkabozu/SideMenu/signout_button.dart';
 import 'package:the4thdayofmikkabozu/user_data.dart' as userData;
@@ -13,6 +17,7 @@ class Sidemenu extends StatefulWidget {
 class SidemenuState extends State<Sidemenu> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _userNameController = TextEditingController();
+  File file;
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -125,6 +130,20 @@ class SidemenuState extends State<Sidemenu> {
               ),
             ],
           ),
+          if (file != null)
+            Container(
+              height: 300,
+              width: 300,
+              child: Image.file(file),
+            ),
+          RaisedButton(
+              child: const Text('アイコン追加'),
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              onPressed: () async {
+                showBottomSheet();
+              }),
           Container(
             child: SignoutButton(),
           ),
@@ -147,5 +166,75 @@ class SidemenuState extends State<Sidemenu> {
         backgroundImage: AssetImage('images/account_circle.png'),
       );
     }
+  }
+
+  Future<int> showCupertinoBottomBar() {
+    //選択するためのボトムシートを表示
+    return showCupertinoModalPopup<int>(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoActionSheet(
+            message: Text('写真をアップロードしますか？'),
+            actions: <Widget>[
+              CupertinoActionSheetAction(
+                child: Text(
+                  'カメラで撮影',
+                ),
+                onPressed: () {
+                  Navigator.pop(context, 0);
+                },
+              ),
+              CupertinoActionSheetAction(
+                child: Text(
+                  'アルバムから選択',
+                ),
+                onPressed: () {
+                  Navigator.pop(context, 1);
+                },
+              ),
+            ],
+            cancelButton: CupertinoActionSheetAction(
+              child: const Text('キャンセル'),
+              onPressed: () {
+                Navigator.pop(context, 2);
+              },
+              isDefaultAction: true,
+            ),
+          );
+        });
+  }
+
+  void showBottomSheet() async {
+    //ボトムシートから受け取った値によって操作を変える
+    final result = await showCupertinoBottomBar();
+    File imageFile;
+    if (result == 0) {
+      imageFile = await ImageUpload(ImageSource.camera).getImageFromDevice();
+    } else if (result == 1) {
+      imageFile = await ImageUpload(ImageSource.gallery).getImageFromDevice();
+    }
+    setState(() {
+      file = imageFile;
+    });
+  }
+}
+
+class ImageUpload {
+  ImageUpload(this.source, {this.quality = 50});
+
+  final ImageSource source;
+  final int quality;
+
+  Future<File> getImageFromDevice() async {
+    // 撮影/選択したFileが返ってくる
+    final imageFile = await ImagePicker().getImage(source: source);
+    // Androidで撮影せずに閉じた場合はnullになる
+    if (imageFile == null) {
+      return null;
+    }
+    //画像を圧縮
+    final File compressedFile = await FlutterNativeImage.compressImage(imageFile.path, quality: quality);
+
+    return compressedFile;
   }
 }
