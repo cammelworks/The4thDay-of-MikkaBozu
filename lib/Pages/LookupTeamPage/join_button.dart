4361c81790f734aeaa47d0a77052f904cd0372e8
Tyanio <tyanio1996@gmail.com>
2021-07-06 17:38:32 +0900

    参加したときにhasNewChatの辞書を追加し、赤ぽちが入った最初からつかないようにlast_visitedも現在時間を登録するようにしました

--- lib/Pages/LookupTeamPage/join_button.dart
+++ lib/Pages/LookupTeamPage/join_button.dart
@@ -32,13 +32,17 @@ class JoinButton extends StatelessWidget {
         .document(_email)
         .setData(<String, dynamic>{'email': _email, 'name': user_data.userName});
 
-    //自分の情報にチームの情報を追加
+    // 自分の情報にチームの情報を追加
+    // last_visitedもここで現在時間を追加する
     Firestore.instance
         .collection('users')
         .document(_email)
         .collection('teams')
         .document(_teamName)
-        .setData(<String, dynamic>{'team_name': _teamName});
+        .setData(<String, dynamic>{
+      'team_name': _teamName,
+      'last_visited': Timestamp.now(),
+    });
 
     // チームの参加人数を取得
     final DocumentSnapshot snapshot = await Firestore.instance.collection('teams').document(_teamName).get();
@@ -46,5 +50,9 @@ class JoinButton extends StatelessWidget {
     // チームの参加人数を1増やしてプッシュ
     final int userNum = (snapshot.data['user_num'] as int) + 1;
     Firestore.instance.collection('teams').document(_teamName).updateData(<String, num>{'user_num': userNum});
+
+    // hasNewChatの辞書に追加
+    // 入った最初からChatの赤ぽちあるのも変な話なので初期値はFalseにしておく
+    user_data.hasNewChat[_teamName] = false;
   }
 }
